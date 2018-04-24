be0186e49c007918911ca5de186a94c627d41ea7
Giles Perry <giles@gilesperry.info>
2018-04-24 12:49:21 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# reset-image-aspect-ratio
+# Reset Image Aspect Ratio plugin for Sketch
+[![Download from Sketchpacks.com](https://badges.sketchpacks.com/plugins/com.gilesperry.reset-image-aspect-ratio/version.svg)](https://api.sketchpacks.com/v1/plugins/com.gilesperry.reset-image-aspect-ratio/download) [![Compatible Sketch Version](https://badges.sketchpacks.com/plugins/com.gilesperry.reset-image-aspect-ratio/compatibility.svg)](https://sketchpacks.com/perrysmotors/reset-image-aspect-ratio)
+
 A Sketch plugin that resets the aspect ratio of selected images.
+
+## Install
+Download or clone the repository, and double click `ResetImageAspectRatio.sketchplugin`.
+
+or...
+
+[![Install PLUGIN NAME with Sketchpacks](http://sketchpacks-com.s3.amazonaws.com/assets/badges/sketchpacks-badge-install.png "Install PLUGIN NAME with Sketchpacks")](https://sketchpacks.com/perrysmotors/reset-image-aspect-ratio/install)
---
+++ ResetImageAspectRatio.sketchplugin/Contents/Sketch/reset-image-aspect-ratio.js
@@ -0,0 +1,33 @@
+var UI = require('sketch/ui'),
+    DOM = require('sketch/dom');
+
+onRun(context);
+
+function onRun(context) {
+  var document = DOM.getSelectedDocument(),
+      selection = document.selectedLayers,
+      images = selection.layers.filter(layer => layer.type === 'Image');
+
+  images.forEach(layer => {
+
+    layer.sketchObject.setConstrainProportions(0);
+
+    let originalSize = layer.image.nsimage.size();
+    let currentAspectRatio = layer.frame.width / layer.frame.height;
+    let originalAspectRatio = originalSize.width / originalSize.height;
+
+    if (currentAspectRatio > originalAspectRatio) {
+      layer.frame = layer.frame.scale(1, currentAspectRatio / originalAspectRatio);
+    } else {
+      layer.frame = layer.frame.scale(originalAspectRatio / currentAspectRatio, 1);
+    }
+
+    layer.sketchObject.setConstrainProportions(1);
+
+  });
+
+  if (images.length === 0) {
+    UI.message('Select one or more images');
+  }
+
+}
