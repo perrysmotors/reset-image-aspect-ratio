2c9e759d98a7c53aa77493134adaa63b95d50644
Giles Perry <giles@gilesperry.info>
2018-05-11 23:44:20 +0100

    Add resetting for rectangles with an image fill

--- ResetImageAspectRatio.sketchplugin/Contents/Sketch/reset-image-aspect-ratio.js
+++ ResetImageAspectRatio.sketchplugin/Contents/Sketch/reset-image-aspect-ratio.js
@@ -91,28 +91,76 @@ var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui"),
 
 function onReset(context) {
   var document = DOM.getSelectedDocument(),
-      selection = document.selectedLayers,
-      images = selection.layers.filter(function (layer) {
-    return layer.type === 'Image';
-  });
-  images.forEach(function (layer) {
-    layer.sketchObject.setConstrainProportions(0);
-    var originalSize = layer.image.nsimage.size();
-    var currentAspectRatio = layer.frame.width / layer.frame.height;
-    var originalAspectRatio = originalSize.width / originalSize.height;
+      selectedLayers = document.selectedLayers.layers;
 
-    if (currentAspectRatio > originalAspectRatio) {
-      layer.frame = layer.frame.scale(1, currentAspectRatio / originalAspectRatio);
-    } else {
-      layer.frame = layer.frame.scale(originalAspectRatio / currentAspectRatio, 1);
+  if (selectedLayers.length > 0) {
+    var bitmaps = selectedLayers.filter(function (layer) {
+      return layer.type === 'Image';
+    });
+    var rectangles = selectedLayers.filter(function (layer) {
+      return isRectangle(layer) && hasPatternFill(layer);
+    });
+    bitmaps.forEach(function (layer) {
+      var originalSize = layer.image.nsimage.size();
+      resetAspectRatio(layer, originalSize);
+    });
+    rectangles.forEach(function (layer) {
+      var originalSize = layer.style.fills[0].sketchObject.image().NSImage().size();
+      resetAspectRatio(layer, originalSize);
+    });
+    var imageCount = bitmaps.length + rectangles.length;
+
+    switch (imageCount) {
+      case 0:
+        UI.message('No images were found');
+        break;
+
+      case 1:
+        UI.message('1 image reset');
+        break;
+
+      default:
+        UI.message("".concat(imageCount, " images reset"));
     }
-
-    layer.sketchObject.setConstrainProportions(1);
-  });
-
-  if (images.length === 0) {
+  } else {
     UI.message('Select one or more images');
   }
+}
+
+function hasPatternFill(layer) {
+  var patternFills = layer.style.fills.filter(function (fill) {
+    return fill.fill === 'Pattern' && fill.enabled;
+  });
+  return patternFills.length === 1;
+}
+
+function isRectangle(layer) {
+  if (layer.type === 'Shape') {
+    var layerCount = layer.sketchObject.layers().count();
+    var layerClass = layer.sketchObject.layers()[0].class();
+
+    if (layerCount === 1 && layerClass === MSRectangleShape) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+function resetAspectRatio(layer, originalSize) {
+  var currentAspectRatio = layer.frame.width / layer.frame.height;
+  var originalAspectRatio = originalSize.width / originalSize.height;
+  layer.sketchObject.setConstrainProportions(0);
+
+  if (currentAspectRatio > originalAspectRatio) {
+    layer.frame = layer.frame.scale(1, currentAspectRatio / originalAspectRatio);
+  } else {
+    layer.frame = layer.frame.scale(originalAspectRatio / currentAspectRatio, 1);
+  }
+
+  layer.sketchObject.setConstrainProportions(1);
 }
 
 /***/ }),
--- src/reset-image-aspect-ratio.js
+++ src/reset-image-aspect-ratio.js
@@ -3,29 +3,74 @@ var UI = require('sketch/ui'),
 
 export function onReset(context) {
   var document = DOM.getSelectedDocument(),
-      selection = document.selectedLayers,
-      images = selection.layers.filter(layer => layer.type === 'Image');
+      selectedLayers = document.selectedLayers.layers;
 
-  images.forEach(layer => {
+  if (selectedLayers.length > 0) {
 
-    layer.sketchObject.setConstrainProportions(0);
+    let bitmaps = selectedLayers.filter(layer => layer.type === 'Image');
+    let rectangles = selectedLayers.filter(layer => isRectangle(layer) && hasPatternFill(layer));
 
-    let originalSize = layer.image.nsimage.size();
-    let currentAspectRatio = layer.frame.width / layer.frame.height;
-    let originalAspectRatio = originalSize.width / originalSize.height;
+    bitmaps.forEach(layer => {
+      let originalSize = layer.image.nsimage.size();
+      resetAspectRatio(layer, originalSize);
+    });
 
-    if (currentAspectRatio > originalAspectRatio) {
-      layer.frame = layer.frame.scale(1, currentAspectRatio / originalAspectRatio);
-    } else {
-      layer.frame = layer.frame.scale(originalAspectRatio / currentAspectRatio, 1);
+    rectangles.forEach(layer => {
+      let originalSize = layer.style.fills[0].sketchObject.image().NSImage().size();
+      resetAspectRatio(layer, originalSize);
+    });
+
+    let imageCount = bitmaps.length + rectangles.length;
+    switch (imageCount) {
+      case 0:
+        UI.message('No images were found');
+        break;
+      case 1:
+        UI.message('1 image reset');
+        break;
+      default:
+        UI.message(`${imageCount} images reset`);
     }
 
-    layer.sketchObject.setConstrainProportions(1);
-
-  });
-
-  if (images.length === 0) {
+  } else {
     UI.message('Select one or more images');
   }
+
+}
+
+function hasPatternFill(layer) {
+  let patternFills = layer.style.fills.filter(fill => fill.fill === 'Pattern' && fill.enabled);
+  return (patternFills.length === 1);
+}
+
+function isRectangle(layer) {
+  if (layer.type === 'Shape') {
+    let layerCount = layer.sketchObject.layers().count();
+    let layerClass = layer.sketchObject.layers()[0].class();
+
+    if (layerCount === 1 && layerClass === MSRectangleShape) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+function resetAspectRatio(layer, originalSize) {
+
+  let currentAspectRatio = layer.frame.width / layer.frame.height;
+  let originalAspectRatio = originalSize.width / originalSize.height;
+
+  layer.sketchObject.setConstrainProportions(0);
+
+  if (currentAspectRatio > originalAspectRatio) {
+    layer.frame = layer.frame.scale(1, currentAspectRatio / originalAspectRatio);
+  } else {
+    layer.frame = layer.frame.scale(originalAspectRatio / currentAspectRatio, 1);
+  }
+
+  layer.sketchObject.setConstrainProportions(1);
 
 }
